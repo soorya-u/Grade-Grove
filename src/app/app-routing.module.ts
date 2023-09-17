17c8aeee0ed58c5777c60d64325e10491241fc62
Soorya U <sooryau7@gmail.com>
2023-09-17 22:56:31 +0530

    Adding About Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './components/home/home.component';
 import { ResultsComponent } from './components/results/results.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   {path:"", component: HomeComponent},
-
+  {path:"about", component: AboutComponent},
   {path:"result/first-sem", component: ResultsComponent},
   {path:"result/first-sem/usn/:id", component: ProfileComponent},
   {path:"result/sec-sem", component: ResultsComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ResultsComponent } from './components/results/results.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ProfileComponent } from './components/profile/profile.component';
     HeaderComponent,
     HomeComponent,
     ResultsComponent,
-    ProfileComponent
+    ProfileComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
