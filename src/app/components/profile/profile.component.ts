e386ba7e2ca25fd40ff9398c53c01b5c03f6baaa
Soorya U <sooryau7@gmail.com>
2023-11-07 19:54:24 +0530

    Added New URL for Profile Pictures

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -5,58 +5,55 @@ import { MarksService } from 'src/app/services/marks.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-
 export class ProfileComponent {
-
-  total_path: string[]
-  sem:string
-  usn: string
-  data: any
-  name:string
-  is_first_or_second:boolean
+  total_path: string[];
+  sem: string;
+  usn: string;
+  data: any;
+  name: string;
+  is_first_or_second: boolean;
 
   constructor(private location: Location, private api: MarksService) {
-    this.total_path = this.location.path().split("/");
+    this.total_path = this.location.path().split('/');
     console.log(this.total_path);
     this.sem = this.total_path[2];
     this.usn = this.total_path[4];
-    this.name="";
-    this.is_first_or_second=false
+    this.name = '';
+    this.is_first_or_second = false;
 
-    if(this.sem === "first-sem") {
-      this.api.getFirstSemMarks().subscribe(res=>{
-        this.data = res
-        this.name="First"
-        this.is_first_or_second=true
-      })
-    }
-    else if(this.sem === "sec-sem") {
-      this.api.getSecondSemMarks().subscribe(res=>{
-        this.data = res
-        this.name="Second"
-        this.is_first_or_second=true
-      })
-    }
-    else{
-      this.api.getThirdSemMarks().subscribe(res=>{
-        this.data = res
-        this.name="Third"
-        this.is_first_or_second=false
-      })
+    if (this.sem === 'first-sem') {
+      this.api.getFirstSemMarks().subscribe((res) => {
+        this.data = res;
+        this.name = 'First';
+        this.is_first_or_second = true;
+      });
+    } else if (this.sem === 'sec-sem') {
+      this.api.getSecondSemMarks().subscribe((res) => {
+        this.data = res;
+        this.name = 'Second';
+        this.is_first_or_second = true;
+      });
+    } else {
+      this.api.getThirdSemMarks().subscribe((res) => {
+        this.data = res;
+        this.name = 'Third';
+        this.is_first_or_second = false;
+      });
     }
   }
 
-  calculateSuperScript(n:number):string {
+  calculateSuperScript(n: number): string {
+    if (n == 1) return 'st';
+    else if (n == 2) return 'nd';
+    else if (n == 3) return 'rd';
+    else return 'th';
+  }
 
-    if(n==1)
-      return "st";
-    else if(n==2)
-      return "nd";
-    else if(n==3)
-      return "rd";
-    else
-      return "th";
+  getProfilePicture(usn: string) {
+    return (
+      'https://alpha-tech-pvt.github.io/TopTenList-API/assets/' + usn + '.jpg'
+    );
   }
 }
