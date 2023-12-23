c9cd4d02c89f39a46a400cc5a3dd582717d7d531
Soorya U <sooryau7@gmail.com>
2023-12-23 12:25:43 +0530

    Modified Services

--- client/src/app/services/marks.service.ts
+++ client/src/app/services/marks.service.ts
@@ -1,26 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-type Subject = {
-  _id: string;
-  subject: string;
-  subject_code: string;
-  int_marks: number;
-  ext_marks: number;
-  total_marks: number;
-};
-
-type Student = {
-  _id: string;
-  name: string;
-  usn: string;
-  subjects: Subject[];
-  total: number;
-  spga: number;
-};
-
-type Rankers = Student[];
+import { Rankers, Student } from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -34,9 +15,12 @@ export class MarksService {
     });
   }
 
-  getUsnResult(sem: string, usn: string) {
-    return this.http.get(`http://localhost:7000/api/v1/${sem}/${usn}`, {
-      responseType: 'json',
-    });
+  getUsnResult(sem: string, usn: string): Observable<Student> {
+    return this.http.get<Student>(
+      `http://localhost:7000/api/v1/${sem}/${usn}`,
+      {
+        responseType: 'json',
+      }
+    );
   }
 }
