bc871678dfc0ddf9290eb2bb6defeb52200a1e37
Soorya U <sooryau7@gmail.com>
2023-12-23 12:05:02 +0530

    Completed API Services

--- client/src/app/services/marks.service.ts
+++ client/src/app/services/marks.service.ts
@@ -1,20 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+type Subject = {
+  _id: string;
+  subject: string;
+  subject_code: string;
+  int_marks: number;
+  ext_marks: number;
+  total_marks: number;
+};
+
+type Student = {
+  _id: string;
+  name: string;
+  usn: string;
+  subjects: Subject[];
+  total: number;
+  spga: number;
+};
+
+type Rankers = Student[];
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MarksService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
+  getSemResult(sem: string): Observable<Rankers> {
+    return this.http.get<Rankers>(`http://localhost:7000/api/v1/${sem}`, {
+      responseType: 'json',
+    });
+  }
 
-  getFirstSemMarks(){
-    return this.http.get("https://alpha-tech-pvt.github.io/TopTenList-API/first_sem.json");
-  }
-  getSecondSemMarks(){
-    return this.http.get("https://alpha-tech-pvt.github.io/TopTenList-API/sec_sem.json");
-  }
-  getThirdSemMarks(){
-    return this.http.get("https://alpha-tech-pvt.github.io/TopTenList-API/third_sem.json");
+  getUsnResult(sem: string, usn: string) {
+    return this.http.get(`http://localhost:7000/api/v1/${sem}/${usn}`, {
+      responseType: 'json',
+    });
   }
 }
