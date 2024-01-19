729c671cce9327de18a3ca7cbf1dd60b86a336ab
Soorya U <sooryau7@gmail.com>
2024-01-19 22:06:15 +0530

    chore #6: Removed Previous Client and Server Directories
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- server/connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-async function connectDatabase(url) {
-  return await mongoose.connect(url, { dbName: "Top-Ten" });
-}
-
-module.exports = { connectDatabase };
