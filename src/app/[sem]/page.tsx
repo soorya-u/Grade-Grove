f62d00794bb584adc49384e7678856b8f4412a5d
Soorya U <sooryau7@gmail.com>
2024-01-30 20:44:23 +0530

    chore #9: Made Semester Page Server Component and Integrated DB Service
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- src/app/[sem]/page.tsx
+++ src/app/[sem]/page.tsx
@@ -4,22 +4,25 @@ import { cn } from "@/utils/shadcn";
 
 import SemTable from "@/components/pages/semester/SemTable";
 
+import { Result as ResultService } from "@/services/results";
+
+import getSuperScript from "@/utils/custom/getSuperScript";
+
 const poppins = Poppins({ weight: "600", subsets: ["latin"] });
 
-const sem = {
-  "first-sem": "First",
-  "second-sem": "Second",
-  "third-sem": "Third",
-};
-
-function Result({ params }: { params: { sem: string } }) {
+async function Result({ params }: { params: { sem: string } }) {
+  const [semester, payload] = await ResultService.getResult(params.sem);
   return (
     <>
       <main className="flex flex-col justify-center items-center gap-7 before:content-[''] after:content-['']">
         <h1 className={cn(poppins.className, "px-4 text-center text-3xl")}>
-          {sem[params.sem as keyof typeof sem]} Semester Top 10 List
+          {semester}
+          <sup className={cn(poppins.className)}>
+            {getSuperScript(semester)}
+          </sup>{" "}
+          Semester Top 10 List
         </h1>
-        <SemTable semester={params.sem} />
+        <SemTable semester={params.sem} data={payload} />
       </main>
     </>
   );
--- src/components/pages/semester/SemTable.tsx
+++ src/components/pages/semester/SemTable.tsx
@@ -1,10 +1,5 @@
-"use client";
-
 import { Rubik, Poppins } from "next/font/google";
-
-import { useEffect, useState } from "react";
-
-import { setReduxRank } from "@/utils/custom/setReduxRank";
+import Link from "next/link";
 
 import {
   Table,
@@ -15,30 +10,20 @@ import {
   TableRow,
 } from "@/components/shadcn/table";
 
-import { useDispatch } from "react-redux";
-
-import { getSemResults } from "@/lib/axios";
-
-import { IRankers } from "@/interface";
 import { cn } from "@/utils/shadcn";
-import Link from "next/link";
-import Loading from "@/app/[sem]/loading";
+
+import { IResultPayload } from "@/interface/result";
 
 const poppins = Poppins({ weight: "600", subsets: ["latin"] });
 const rubik = Rubik({ weight: "400", subsets: ["latin"] });
 
-function SemTable({ semester }: { semester: string }) {
-  const [result, setResult] = useState<IRankers>([]);
-  const [loading, setLoading] = useState(false);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    setLoading(true);
-    getSemResults(semester)
-      .then((res) => setResult(res.data))
-      .finally(() => setLoading(false));
-  }, []);
+async function SemTable({
+  semester,
+  data,
+}: {
+  semester: string;
+  data: IResultPayload[];
+}) {
   return (
     <>
       <Table className="w-[95vw] sm:w-[80vw]">
@@ -77,69 +62,57 @@ function SemTable({ semester }: { semester: string }) {
             </TableHead>
           </TableRow>
         </TableHeader>
-        {!loading ? (
-          <TableBody>
-            {result.map((elem, idx) => (
-              <TableRow key={elem._id}>
-                <TableCell
-                  className={cn(
-                    rubik.className,
-                    "font-medium text-center text-base"
-                  )}
-                >
-                  {idx + 1}
-                </TableCell>
-                <TableCell
-                  className={cn(
-                    rubik.className,
-                    "text-center cursor-pointer text-base"
-                  )}
-                  style={{
-                    fontStyle: "italic",
-                    textDecoration: "underline",
-                    textUnderlineOffset: 3,
-                  }}
-                >
-                  <Link
-                    href={`/${semester}/${elem.usn}`}
-                    onClick={() => setReduxRank(idx + 1, dispatch)}
-                  >
-                    {elem.name}
-                  </Link>
-                </TableCell>
-                <TableCell
-                  className={cn(
-                    rubik.className,
-                    "hidden md:table-cell text-center text-base"
-                  )}
-                >
-                  {elem.usn}
-                </TableCell>
-                <TableCell
-                  className={cn(rubik.className, "text-center text-base")}
-                >
-                  {elem.total}
-                </TableCell>
-                <TableCell
-                  className={cn(
-                    rubik.className,
-                    "hidden md:table-cell text-center text-base"
-                  )}
-                >
-                  {elem.spga}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        ) : (
-          <TableBody>
-            {Array(10)
-              .fill(0)
-              .map((_, idx) => (
-                <Loading key={idx} />
-              ))}
-          </TableBody>
-        )}
+
+        <TableBody>
+          {data.map((elem, idx) => (
+            <TableRow key={idx}>
+              <TableCell
+                className={cn(
+                  rubik.className,
+                  "font-medium text-center text-base"
+                )}
+              >
+                {idx + 1}
+              </TableCell>
+              <TableCell
+                className={cn(
+                  rubik.className,
+                  "text-center cursor-pointer text-base"
+                )}
+                style={{
+                  fontStyle: "italic",
+                  textDecoration: "underline",
+                  textUnderlineOffset: 3,
+                }}
+              >
+                <Link href={`/${semester}/${elem.usn}`}>
+                  {elem.studentName}
+                </Link>
+              </TableCell>
+              <TableCell
+                className={cn(
+                  rubik.className,
+                  "hidden md:table-cell text-center text-base"
+                )}
+              >
+                {elem.usn}
+              </TableCell>
+              <TableCell
+                className={cn(rubik.className, "text-center text-base")}
+              >
+                {elem.totalMarks}
+              </TableCell>
+              <TableCell
+                className={cn(
+                  rubik.className,
+                  "hidden md:table-cell text-center text-base"
+                )}
+              >
+                {elem.sgpa}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
       </Table>
     </>
   );
