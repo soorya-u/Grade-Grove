09c1c9e83440d86fa665fdb13ea11ad51e458640
Soorya U <sooryau7@gmail.com>
2024-01-31 00:46:31 +0530

    refactor: Removed Unnecessary Files and Dependencies
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- src/hooks/redux/rank/index.ts
+++
@@ -1,9 +0,0 @@
-
-import { useSelector } from "react-redux";
-import { RootState } from "@/lib/redux/store";
-
-
-export const useReduxRankGetter = () => {
-  const rank = useSelector((state: RootState) => state.rank);
-  return rank;
-};
