35bca58f38a9539d38d5b5811b1eb335d8d9a8f8
Soorya U <sooryau7@gmail.com>
2024-03-03 10:59:28 +0530

    fix: Changed Styles of Landing Page
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,57 @@
 import Image from "next/image";
-import { Audiowide, Poppins } from "next/font/google";
+import { Noto_Sans, Poppins } from "next/font/google";
 
 import { cn } from "@/utils/shadcn";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "@/components/shadcn/button";
+import Link from "next/link";
 
-const audiowide = Audiowide({ weight: "400", subsets: ["latin"] });
+const natoSans = Noto_Sans({ weight: "500", subsets: ["latin"] });
 
 const poppins = Poppins({ weight: "600", subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center items-center gap-6 flex-wrap px-4">
-        <h1 className={cn(audiowide.className, "text-5xl text-center")}>
-          elite-AIML
-        </h1>
+      <section className="flex flex-col justify-center items-center gap-6 max-w-full flex-wrap mt-16">
         <Image
           priority
-          src="/elite_aiml.png"
+          src="/elite_aiml_white.png"
           alt="logo"
           height={7 * 16}
           width={7 * 16}
-          className="invert"
         ></Image>
-      </div>
-      <article className="text-center text-xl px-4">
-        Our Website is Dedicated for Providing a Comprehensive List of AIML
-        students who have Achieved the Highest Scores and Good Rankings in the
-        Semester Examinations. We typically Aim to Recognize and Celebrate the
-        Accomplishments of Top Performers in the Respective Subjects. Users can
-        Access Information about the Leading Achievers, their Achievements, and
-        often find Resources or Tips for Aspiring Individuals looking to excel
-        in their Chosen Pursuits. Toppers List websites serve as a source of
-        inspiration and information for those striving for excellence.
-      </article>
-
-      <article className="text-center text-xl px-4">
-        Academic Excellence is a pursuit that has been Cherished across Cultures
-        and Generations. Recognizing and celebrating the achievements of
-        academic toppers has become a tradition in educational Institutions
-        Worldwide. The Academic Toppers List serves as a Testament to the
-        Dedication, Hardwork, and Exceptional Abilities of these students.
-      </article>
-
-      <h2
-        className={cn(
-          poppins.className,
-          "text-3xl text-center self-center px-4"
-        )}
-      >
-        Significance of Academic Toppers Lists
-      </h2>
-      <ul className="flex flex-col gap-2 px-4">
-        <li className="list-none text-lg">
-          <FontAwesomeIcon className="w-3 inline mr-2" icon={faArrowRight} />{" "}
-          Fostering Healthy Competition
-        </li>
-        <li className="list-none text-lg">
-          <FontAwesomeIcon className="w-3 inline mr-2" icon={faArrowRight} />{" "}
-          Acknowledging Achievements
-        </li>
-        <li className="list-none text-lg">
-          <FontAwesomeIcon className="w-3 inline mr-2" icon={faArrowRight} />{" "}
-          Encouraging Excellence
-        </li>
-      </ul>
+        <h1
+          className={cn(
+            "text-6xl text-white text-wrap text-center w-10/12",
+            poppins.className
+          )}
+        >
+          A Central Hub for all your Results{" "}
+        </h1>
+        <h2
+          className={cn(
+            natoSans.className,
+            "text-xl text-center text-[#ccccd2] w-2/3"
+          )}
+        >
+          Take a panoramic dive into your academic progress by effortlessly
+          reviewing, analyzing and tracking every mark and grade earned
+          throughout each semester.
+        </h2>
+        <div className="flex flex-wrap justify-center items-center gap-x-10 gap-y-5">
+          <Button variant="outline">
+            <Link href="#">Check out your Result</Link>
+          </Button>
+          <Button variant="outline">
+            <Link href="https://github.com/soorya-u/Belief">
+              View Source Code
+            </Link>
+          </Button>
+        </div>
+      </section>
     </>
   );
 }
--- src/components/shadcn/button.tsx
+++ src/components/shadcn/button.tsx
@@ -1,9 +1,10 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/utils/shadcn";
+import { Poppins } from "next/font/google";
 
-import { cn } from "@/utils/shadcn"
-
+const poppins = Poppins({ weight: "600", subsets: ["latin"] });
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
@@ -12,10 +13,12 @@ const buttonVariants = cva(
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        outline: cn(
+          poppins.className,
+          "border-[2px] border-white bg-transparent hover:bg-white hover:text-[#931D68] text-md"
+        ),
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80", 
         ghost: "hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
@@ -31,26 +34,26 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
