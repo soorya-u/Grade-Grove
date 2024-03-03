5ecfc634a68c1bed119cb832876f17101f914a5a
Soorya U <sooryau7@gmail.com>
2024-03-03 11:16:44 +0530

    chore: Added Button to Header
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- src/components/custom/Header/index.tsx
+++ src/components/custom/Header/index.tsx
@@ -1,27 +1,38 @@
 import Link from "next/link";
-import { Audiowide } from "next/font/google";
+import { Audiowide, Poppins } from "next/font/google";
 
 import Navigator from "./Navigator";
 import Hamburger from "./Hamburger";
+import { Button } from "@/components/shadcn/button";
+import { cn } from "@/utils/shadcn";
 
 const audiowide = Audiowide({ weight: "400", subsets: ["latin"] });
+const poppins = Poppins({ weight: "600", subsets: ["latin"] });
 
 function Header() {
   return (
     <>
       <header className="sticky top-0 w-full z-50">
-        <nav className="h-[10vh] flex justify-between items-center p-2 backdrop-blur-sm border-b border-[#ffffff49] rounded-b-md gap-8 px-10">
-          <Navigator className="hidden md:flex" />
-          <Hamburger className="block md:hidden" />
-          <section className="flex justify-end">
+        <nav className="h-[10vh] flex justify-between items-center p-2 backdrop-blur-sm border-b border-[#ffffff49] rounded-b-md px-10">
+          <div className="flex justify-start items-center gap-8">
+            <Hamburger className="block md:hidden" />
             <Link
               href="https://github.com/soorya-u/Elite-AIML"
-              className={`${audiowide.className} text-3xl md:text-4xl text-nowrap`}
+              className={cn(
+                audiowide.className,
+                "text-2xl text-nowrap hidden xs:block"
+              )}
               target="_blank"
             >
               elite-AIML
             </Link>
-          </section>
+            <Navigator className="hidden md:flex" />
+          </div>
+          <div className="ml-8">
+            <Button size="sm" className={cn(poppins.className, "text-md")}>
+              Login
+            </Button>
+          </div>
         </nav>
       </header>
     </>
