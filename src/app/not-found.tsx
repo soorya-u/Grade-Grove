d326637b202c0010c0d822f0ffb0a07d29d8f849
Soorya U <sooryau7@gmail.com>
2024-03-16 16:25:07 +0530

    feat: Added Not Found Page
    Signed-off-by: Soorya U <sooryau7@gmail.com>

---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/shadcn/button";
+import { cn } from "@/utils/shadcn";
+import { Noto_Sans, Poppins } from "next/font/google";
+import Link from "next/link";
+
+const natoSans = Noto_Sans({ weight: "500", subsets: ["latin"] });
+
+const poppins = Poppins({ weight: "600", subsets: ["latin"] });
+
+export default function NotFound() {
+  return (
+    <section className="flex flex-col justify-center items-center gap-10 my-14 before:content-[''] after:content-['']">
+      <h2 className={cn("text-7xl text-center px-3", poppins.className)}>
+        Error 404: Page Not Found
+      </h2>
+      <p className={cn("text-3xl text-center px-3", natoSans.className)}>
+        Could not find requested resource
+      </p>
+      <Button variant={"outline"}>
+        <Link className={cn("text-lg text-center", natoSans.className)} href="/">Return Home</Link>
+      </Button>
+    </section>
+  );
+}
