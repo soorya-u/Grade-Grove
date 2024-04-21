db5983768771f599c422840bdbb3e0c3d92a0206
Soorya U <sooryau7@gmail.com>
2024-04-21 15:15:37 +0530

    chore: Fixed Errors
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- src/components/custom/Auth/OAuthButtons.tsx
+++ src/components/custom/Auth/OAuthButtons.tsx
@@ -9,6 +9,7 @@ import { cn } from "@/utils/cn";
 import { Button } from "@/components/primitives/button";
 import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const poppins = Poppins({ weight: "600", subsets: ["latin"] });
 
@@ -21,39 +22,41 @@ export default function OAuthButtons() {
   const searchPrarms = useSearchParams();
 
   return (
-    <div className="flex flex-col justify-center items-center gap-4 xs:flex-row">
-      <Button
-        onClick={async () =>
-          await handleClick("google", searchPrarms.get("callbackUrl"))
-        }
-        variant="outline"
-        className={cn(
-          poppins.className,
-          "flex gap-x-2 text-sm hover:bg-white hover:text-[#931D68] group"
-        )}
-      >
-        <FontAwesomeIcon
-          icon={faGoogle}
-          className="w-5 h-5 group-hover:[&_path]:fill-[#931D68]"
-        />
-        Contiue with Google
-      </Button>
-      <Button
-        onClick={async () =>
-          await handleClick("github", searchPrarms.get("callbackUrl"))
-        }
-        variant="outline"
-        className={cn(
-          poppins.className,
-          "flex gap-x-2 text-sm hover:bg-white hover:text-[#931D68] group"
-        )}
-      >
-        <FontAwesomeIcon
-          icon={faGithub}
-          className="w-6 h-6 group-hover:[&_path]:fill-[#931D68]"
-        />
-        Contiue with Github
-      </Button>
-    </div>
+    <Suspense>
+      <div className="flex flex-col justify-center items-center gap-4 xs:flex-row">
+        <Button
+          onClick={async () =>
+            await handleClick("google", searchPrarms.get("callbackUrl"))
+          }
+          variant="outline"
+          className={cn(
+            poppins.className,
+            "flex gap-x-2 text-sm hover:bg-white hover:text-[#931D68] group"
+          )}
+        >
+          <FontAwesomeIcon
+            icon={faGoogle}
+            className="w-5 h-5 group-hover:[&_path]:fill-[#931D68]"
+          />
+          Contiue with Google
+        </Button>
+        <Button
+          onClick={async () =>
+            await handleClick("github", searchPrarms.get("callbackUrl"))
+          }
+          variant="outline"
+          className={cn(
+            poppins.className,
+            "flex gap-x-2 text-sm hover:bg-white hover:text-[#931D68] group"
+          )}
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            className="w-6 h-6 group-hover:[&_path]:fill-[#931D68]"
+          />
+          Contiue with Github
+        </Button>
+      </div>
+    </Suspense>
   );
 }
