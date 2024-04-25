e02f38f6834087e8b800517c8c1acba3577581d4
Soorya U <sooryau7@gmail.com>
2024-04-25 22:32:49 +0530

    fix: Minor Fixes 2
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,13 +1,12 @@
-import { User } from "@prisma/client";
-import NextAuth from "next-auth/next";
-import { JWT } from "next-auth/jwt";
+import { $Enums } from "@prisma/client";
+import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
-    user: User;
+    user: User & DefaultSession["user"];
   }
-}
 
-declare module "next-auth/jwt" {
-  type JWT = User;
+  interface User {
+    role: $Enums.Role;
+  }
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,15 +3,32 @@ import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { Adapter } from "next-auth/adapters";
 import prisma from "./db";
 import env from "@/schema/env";
+import { cache } from "react";
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
+const {
+  auth: nextAuth,
+  handlers,
+  signIn,
+  signOut,
+} = NextAuth({
   // TODO: fix ENV
   secret: "ABCD",
-  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session: ({ session, user }) => {
+      session.user.role = user.role;
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [Google, GitHub],
   pages: {
     signIn: "/auth/signup",
   },
 });
+
+const auth = cache(nextAuth);
+
+export { handlers, signIn, signOut, auth };
